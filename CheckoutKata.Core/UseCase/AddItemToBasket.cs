5b7dfb522365d0385695347007a04241a6fe55f7
Jamie Kitson <jamie.kitson@davies-group.com>
2023-10-29 21:00:20 +0000

    Add project files.

---
+++ CheckoutKata.Core/UseCase/AddItemToBasket.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata.Core.UseCase
+{
+    internal class AddItemToBasket
+    {
+    }
+}
